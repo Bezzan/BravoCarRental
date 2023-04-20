747ee42d8477b528780c85ca14a6202286098854
Mikael Eriksson <m.eriksson@gmx.com>
2023-04-20 15:10:10 +0200

    TODO Skeleton for what the CustomerController class might need.

---
+++ src/main/java/com/bravo/gruppbravowigellkoncernencarrental/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package com.bravo.gruppbravowigellkoncernencarrental.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <code>CustomerController</code> - Some CRUD commands for customer
+ * @authors Mikael Eriksson (mikael.eriksson@edu.edugrade.se)
+ * @version 0.0.1
+ */
+
+@RestController
+public class CustomerController {
+
+    // -----------------------------------------------------------------------------------------------------------------
+    //   Properties
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // TODO - Needs CustomerService class, so services from that class can be done.
+
+    // -----------------------------------------------------------------------------------------------------------------
+    //   Methods CRUD
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // TODO - Needs @GetMapping, for "api/v1/cars", so customer service gets all cars in database
+
+    // TODO - Needs @PostMapping, for "api/v1/ordercar", so customer service can order a car from database
+
+    // TODO - Needs @PutMapping, for "api/v1/cancelorder", so customer service can cancel a order that they made
+
+    // TODO - Needs @GetMapping, for "api/v1/myorders", so customer service can get all orders they done
+}
